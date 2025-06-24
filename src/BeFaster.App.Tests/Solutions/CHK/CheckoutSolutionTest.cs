0040eddbad32f25eb5a6d764f31d81bd8eea1ff9
sbx_user1051 <sbx_user1051@169.254.46.17>
2025-06-24 08:46:56 +0000

    Tue Jun 24 08:46:56 UTC 2025

--- src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolutionTest.cs
@@ -6,6 +6,11 @@ namespace BeFaster.App.Tests.Solutions.CHK
     [TestFixture]
     public class CheckoutSolutionTest
     {
-
+        [TestCase()]
+        public int Checkout(string? skus)
+        {
+            return new CheckoutSolution().Checkout(skus);
+        }
     }
 }
+
--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -7,6 +7,13 @@ namespace BeFaster.App.Solutions.CHK
         public int Checkout(string? skus)
         {
             throw new SolutionNotImplementedException();
+            // if null or empty return -1
+            // maintain a dictionary to hold the SKU prices and special offers
+            // maintain a dictionary to hold the SKU special offers counts
+            // maintain a dictionary to hold the SKU counts
+            // go through the skus string, count the occurrences of each SKU
+            // if an unknown SKU is found return -1
         }
     }
 }
+
